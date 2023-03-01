ba4e188400db79504cae2b89a9f5e96d3e5aa6dc
K3V1N-G <123893629+K3V1N-G@users.noreply.github.com>
2023-03-01 13:01:56 -0600

    Code now validates and creates new user

--- register.php
+++ register.php
@@ -1,43 +1,46 @@
 <?php
 	$servername = "localhost";
-	$username = "root";
-	$password = "GMG2023";
-	$dbname = "test";
+	$username = "id20384193_option1";
+	$password = "1RK{nX*b?^6g-}<s";
+	$dbname = "id20384193_test";
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
 
-	// Check connection
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
+// Check if connection was successful
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-	// If form is submitted, insert values into database
-	if(isset($_POST['submit'])) {
-		
-		$fullname = $_POST['fullname'];
-		$username = $_POST['username'];
-		$email = $_POST['email'];
-		$password = $_POST['password'];
-		$role = $_POST['role'];
-		
-		if(empty($errors)) {
-			$sql = "INSERT INTO users_table (fullname, username, email, password, role) VALUES ('$fullname', '$username', '$email', '$password', '$role')";
+// Retrieve the form data
+$username = $_POST['username'];
+$email = $_POST['email'];
+$fullname = $_POST['fullname'];
+$role = $_POST['role'];
+$password = $_POST['password'];
+$confirm_password = $_POST['confirm_password'];
 
-			if ($conn->query($sql) === TRUE) {
-				// Redirect to home page
-				header("Location: home.php");
-				exit();
-			} else {
-				echo "Error: " . $sql . "<br>" . $conn->error;
-			}
-		} else {
-			// Display errors if there are any
-			foreach($errors as $error) {
-				echo "<p>$error</p>";
-			}
-		}
-	}
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	$error = "Invalid email format";
+}
 
+// Validate the form data
+if ($password != $confirm_password) {
+    echo "Passwords do not match. Please try again.";
+} else {
+    // Prepare the SQL statement to insert the new user data
+    $sql = "INSERT INTO users_table (fullname, username, email, password, role) VALUES ('$fullname', '$username', '$email', '$password', '$role')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New user registered successfully";
+    } else {
+        $conn->close();
+        $error = "Error: " . $sql . "<br>" . $conn->error;
+        header("Location: register.php?error=" . urlencode($error));
+        exit();
+    }
+}
+
+// Close the database connection
 $conn->close();
 ?>
