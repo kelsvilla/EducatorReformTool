d27f25b62b96117241fb494ceeb489a88b0c3252
Kelsee Villareal <kelseevillareal@my.unt.edu>
2023-03-23 17:11:07 -0500

    still fixing issue 4

--- register.php
+++ register.php
@@ -33,7 +33,7 @@ else {
 		else if($role == "Student"){
 			header("Location: homepage.php");
 		}
-        header("Location: home.php");
+        // header("Location: home.php");
         exit();
     } else {
         $conn->close();
